0371ee786c4e31ae7fb48af1e35e7800c32fa0ad
dev-paulocoelho-admin <contato.paulomedina@outlook.com>
2025-08-27 11:41:12 -0300

    Refactor - update route names for produtos, fornecedores, and clientes

--- routes/web.php
+++ routes/web.php
@@ -19,38 +19,38 @@ Route::prefix('/app')->group(function () {
     // Produtos
     // ===============================
     Route::controller(ProdutoController::class)->prefix('produtos')->group(function () {
-        Route::get('/', 'index')->name('app.produtos.index');
-        Route::get('/create', 'create')->name('app.produtos.create');
-        Route::post('/', 'store')->name('app.produtos.store');
-        Route::get('/{id}', 'show')->where('id', '[0-9]+')->name('app.produtos.show');
-        Route::get('/{id}/edit', 'edit')->where('id', '[0-9]+')->name('app.produtos.edit');
-        Route::put('/{id}', 'update')->where('id', '[0-9]+')->name('app.produtos.update');
-        Route::delete('/{id}', 'destroy')->where('id', '[0-9]+')->name('app.produtos.destroy');
+        Route::get('/', 'index')->name('produtos.index');
+        Route::get('/create', 'create')->name('produtos.create');
+        Route::post('/', 'store')->name('produtos.store');
+        Route::get('/{id}', 'show')->where('id', '[0-9]+')->name('produtos.show');
+        Route::get('/{id}/edit', 'edit')->where('id', '[0-9]+')->name('produtos.edit');
+        Route::put('/{id}', 'update')->where('id', '[0-9]+')->name('produtos.update');
+        Route::delete('/{id}', 'destroy')->where('id', '[0-9]+')->name('produtos.destroy');
     });
 
     // ===============================
     // Fornecedores
     // ===============================
     Route::controller(FornecedorController::class)->prefix('fornecedores')->group(function () {
-        Route::get('/', 'index')->name('app.fornecedores.index');
-        Route::get('/create', 'create')->name('app.fornecedores.create');
-        Route::post('/', 'store')->name('app.fornecedores.store');
-        Route::get('/{id}', 'show')->where('id', '[0-9]+')->name('app.fornecedores.show');
-        Route::get('/{id}/edit', 'edit')->where('id', '[0-9]+')->name('app.fornecedores.edit');
-        Route::put('/{id}', 'update')->where('id', '[0-9]+')->name('app.fornecedores.update');
-        Route::delete('/{id}', 'destroy')->where('id', '[0-9]+')->name('app.fornecedores.destroy');
+        Route::get('/', 'index')->name('fornecedores.index');
+        Route::get('/create', 'create')->name('fornecedores.create');
+        Route::post('/', 'store')->name('fornecedores.store');
+        Route::get('/{id}', 'show')->where('id', '[0-9]+')->name('fornecedores.show');
+        Route::get('/{id}/edit', 'edit')->where('id', '[0-9]+')->name('fornecedores.edit');
+        Route::put('/{id}', 'update')->where('id', '[0-9]+')->name('fornecedores.update');
+        Route::delete('/{id}', 'destroy')->where('id', '[0-9]+')->name('fornecedores.destroy');
     });
 
     // ===============================
     // Clientes
     // ===============================
     Route::controller(ClienteController::class)->prefix('clientes')->group(function () {
-        Route::get('/', 'index')->name('app.clientes.index');
-        Route::get('/create', 'create')->name('app.clientes.create');
-        Route::post('/', 'store')->name('app.clientes.store');
-        Route::get('/{id}', 'show')->where('id', '[0-9]+')->name('app.clientes.show');
-        Route::get('/{id}/edit', 'edit')->where('id', '[0-9]+')->name('app.clientes.edit');
-        Route::put('/{id}', 'update')->where('id', '[0-9]+')->name('app.clientes.update');
-        Route::delete('/{id}', 'destroy')->where('id', '[0-9]+')->name('app.clientes.destroy');
+        Route::get('/', 'index')->name('clientes.index');
+        Route::get('/create', 'create')->name('clientes.create');
+        Route::post('/', 'store')->name('clientes.store');
+        Route::get('/{id}', 'show')->where('id', '[0-9]+')->name('clientes.show');
+        Route::get('/{id}/edit', 'edit')->where('id', '[0-9]+')->name('clientes.edit');
+        Route::put('/{id}', 'update')->where('id', '[0-9]+')->name('clientes.update');
+        Route::delete('/{id}', 'destroy')->where('id', '[0-9]+')->name('clientes.destroy');
     });
 });
